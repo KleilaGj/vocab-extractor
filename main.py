8a25d036c753ca685b6a3bce4e02ded48b647fd6
Kleila Gjeta <kleila.gjeta@gmail.com>
2026-06-21 19:19:48 +0100

    Added word frequency counting

--- main.py
+++ main.py
@@ -9,4 +9,13 @@ for char in string.punctuation:
 
 words = text.split()
 
+freq = {}
+
+for word in words:
+    if word in freq:
+        freq[word] += 1
+    else:
+        freq[word] = 1
+
 print(words)
+print(freq)
